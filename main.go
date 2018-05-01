bda134607b9123651347dfa35e40666eaf74807d
yoazm <yoazm@jfrog.com>
2018-05-01 14:16:40 +0300

    make hello as lib

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
-package main
+package vgohelloworld
 
 import "fmt"
 import log "github.com/sirupsen/logrus"
-
-func main(){
-    fmt.Println("Hell from main")
-    Hello()
-}
 
 func Hello(){
     fmt.Println("hello from Hello functions")
