ebb51da3c187188bc5db08ba30e68d49cbf42682
yoazm <yoazm@jfrog.com>
2018-05-01 12:14:13 +0300

    Add exported library funtcion

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import "fmt"
 import log "github.com/sirupsen/logrus"
+
 func main(){
-    fmt.Println("hello world")
+    fmt.Println("Hell from main")
+    Hello()
+}
+
+func Hello(){
+    fmt.Println("hello from Hello functions")
     log.Info("this log is printed from logrus dependency")
 }
+
+
 
